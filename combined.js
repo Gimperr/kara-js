e9ffaa63ec5e58869717038cef565771f22d5d80
Tomasz Dzialowy <gimper@karachan.org>
2013-12-09 13:56:26 +0100

    do the captcha earlier

--- combined.js
+++ combined.js
@@ -1546,7 +1546,7 @@ if (v === 0) {
 		}
 	});
 };
-$(function(){
+DOMContentLoaded("DOMContentLoaded", function(){
                                 $("#captchaField").before("<div style='width: 300px; height: 70px; background-color: white;'><a href='#' id='captchaClickHere' style='vertical-align: middle; align: center;'>Click here</a></div>");
                                 $("#captchaClickHere").click(function (event) {
                                         event.preventDefault();
@@ -1559,4 +1559,4 @@ $(function(){
                                         });
                                         $(this).parent().hide();
                                 });
-});
+}, false);
